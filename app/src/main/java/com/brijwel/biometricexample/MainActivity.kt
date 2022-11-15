84572ac9b7a7c8e2d7c5daf73d2e59ef7d241eee
Brijwel <rbrijwel@gmail.com>
2022-11-15 17:36:09 +0530

    initial commit

---
+++ app/src/main/java/com/brijwel/biometricexample/MainActivity.kt
@@ -0,0 +1,126 @@
+package com.brijwel.biometricexample
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricManager.Authenticators.*
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
+import java.util.concurrent.Executor
+
+class MainActivity : AppCompatActivity() {
+
+    private val executor: Executor by lazy { ContextCompat.getMainExecutor(this) }
+
+    private val biometricManager: BiometricManager by lazy { BiometricManager.from(this) }
+
+    private val promptInfo: BiometricPrompt.PromptInfo by lazy {
+        BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Biometric login for my app")
+            .setSubtitle("Log in using your biometric credential")
+            .setAllowedAuthenticators(BIOMETRIC_STRONG or BIOMETRIC_WEAK or DEVICE_CREDENTIAL)
+            .build()
+    }
+
+    private val biometricPrompt: BiometricPrompt by lazy {
+        BiometricPrompt(this, executor, biometricCallback)
+
+    }
+
+
+    private val biometricCallback = object : BiometricPrompt.AuthenticationCallback() {
+        override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+            super.onAuthenticationError(errorCode, errString)
+            when (errorCode) {
+                BiometricPrompt.ERROR_LOCKOUT -> {
+                    toast("Locked out due to too many attempts. This occurs after 5 failed attempts, and lasts for 30 seconds.")
+                }
+                BiometricPrompt.ERROR_LOCKOUT_PERMANENT -> {
+                    toast("Biometric authentication is disabled until the user unlocks with their device credential.")
+                }
+                BiometricPrompt.ERROR_NEGATIVE_BUTTON -> {
+                    toast("Biometric cancelled by user.")
+                }
+                BiometricPrompt.ERROR_TIMEOUT -> {
+                    toast("Biometric authentication timeout.")
+                }
+                BiometricPrompt.ERROR_USER_CANCELED -> {
+                    toast("Biometric cancelled by user.")
+                }
+                else -> {
+                    toast("something went wrong.")
+                }
+            }
+        }
+
+        override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+            super.onAuthenticationSucceeded(result)
+            toast("Biometric authentication success")
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<AppCompatImageView>(R.id.authentication).setOnClickListener {
+            authenticate()
+        }
+    }
+
+    private fun authenticate() {
+        when (biometricManager.canAuthenticate(BIOMETRIC_STRONG or BIOMETRIC_WEAK or DEVICE_CREDENTIAL)) {
+            BiometricManager.BIOMETRIC_SUCCESS -> {
+                biometricPrompt.authenticate(promptInfo)
+            }
+            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE ->
+                toast("No biometric features available on this device.")
+            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE ->
+                toast("Biometric features are currently unavailable.")
+            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> {
+                toast("Biometric feature available but not enrolled.")
+                // Prompts the user to create credentials that your app accepts.
+                biometricEnrollmentRequest.launch(getFingerPrintEnrollmentIntent())
+            }
+            else -> toast("something went wrong.")
+        }
+
+    }
+
+    private val biometricEnrollmentRequest =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == Activity.RESULT_OK) {
+
+            }
+        }
+}
+
+private fun getFingerPrintEnrollmentIntent(): Intent {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        Intent(Settings.ACTION_BIOMETRIC_ENROLL).apply {
+            putExtra(
+                Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED,
+                BIOMETRIC_STRONG or DEVICE_CREDENTIAL
+            )
+        }
+    } else {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            @Suppress("DEPRECATION")
+            Intent(Settings.ACTION_FINGERPRINT_ENROLL)
+        } else {
+            Intent(Settings.ACTION_SECURITY_SETTINGS)
+        }
+    }
+}
+
+private fun Context.toast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
