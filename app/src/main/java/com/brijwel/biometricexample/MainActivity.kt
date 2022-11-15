30d72eea41da27b14fd40ea1367bb0d67739aca7
Brijwel <rbrijwel@gmail.com>
2022-11-15 20:24:33 +0530

    refactor

--- app/src/main/java/com/brijwel/biometricexample/MainActivity.kt
+++ app/src/main/java/com/brijwel/biometricexample/MainActivity.kt
@@ -1,14 +1,13 @@
 package com.brijwel.biometricexample
 
-import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.biometric.BiometricManager
@@ -27,8 +26,7 @@ class MainActivity : AppCompatActivity() {
         BiometricPrompt.PromptInfo.Builder()
             .setTitle("Biometric login")
             .setSubtitle("Log in using your biometric credential")
-            .setAllowedAuthenticators(authenticators)
-            .build()
+            .setAllowedAuthenticators(authenticators).build()
     }
 
     private val biometricPrompt: BiometricPrompt by lazy {
@@ -42,24 +40,14 @@ class MainActivity : AppCompatActivity() {
         override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
             super.onAuthenticationError(errorCode, errString)
             when (errorCode) {
-                BiometricPrompt.ERROR_LOCKOUT -> {
+                BiometricPrompt.ERROR_LOCKOUT ->
                     toast("Locked out due to too many attempts. This occurs after 5 failed attempts, and lasts for 30 seconds.")
-                }
-                BiometricPrompt.ERROR_LOCKOUT_PERMANENT -> {
+                BiometricPrompt.ERROR_LOCKOUT_PERMANENT ->
                     toast("Biometric authentication is disabled until the user unlocks with their device credential.")
-                }
-                BiometricPrompt.ERROR_NEGATIVE_BUTTON -> {
-                    toast("Biometric cancelled by user.")
-                }
-                BiometricPrompt.ERROR_TIMEOUT -> {
-                    toast("Biometric authentication timeout.")
-                }
-                BiometricPrompt.ERROR_USER_CANCELED -> {
-                    toast("Biometric cancelled by user.")
-                }
-                else -> {
-                    toast("something went wrong.")
-                }
+                BiometricPrompt.ERROR_NEGATIVE_BUTTON -> toast("Biometric cancelled by user.")
+                BiometricPrompt.ERROR_TIMEOUT -> toast("Biometric authentication timeout.")
+                BiometricPrompt.ERROR_USER_CANCELED -> toast("Biometric cancelled by user.")
+                else -> toast("something went wrong.")
             }
         }
 
@@ -83,13 +71,9 @@ class MainActivity : AppCompatActivity() {
 
     private fun authenticate() {
         when (biometricManager.canAuthenticate(authenticators)) {
-            BiometricManager.BIOMETRIC_SUCCESS -> {
-                biometricPrompt.authenticate(promptInfo)
-            }
-            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE ->
-                toast("No biometric features available on this device.")
-            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE ->
-                toast("Biometric features are currently unavailable.")
+            BiometricManager.BIOMETRIC_SUCCESS -> biometricPrompt.authenticate(promptInfo)
+            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE -> toast("No biometric features available on this device.")
+            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE -> toast("Biometric features are currently unavailable.")
             BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> {
                 toast("Biometric feature available but not enrolled.")
                 // Prompts the user to create credentials that your app accepts.
@@ -102,9 +86,7 @@ class MainActivity : AppCompatActivity() {
 
     private val biometricEnrollmentRequest =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-            if (it.resultCode == Activity.RESULT_OK) {
-                updateBioMetricStatus()
-            }
+            updateBioMetricStatus()
         }
 
     private fun updateBioMetricStatus() {
@@ -113,27 +95,13 @@ class MainActivity : AppCompatActivity() {
 
     private fun getBioMetricStatus(): String {
         return when (biometricManager.canAuthenticate(authenticators)) {
-            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE -> {
-                "Hardware Unavailable"
-            }
-            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> {
-                "Biometric None Enrolled"
-            }
-            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE -> {
-                "No Biometric Hardware"
-            }
-            BiometricManager.BIOMETRIC_ERROR_SECURITY_UPDATE_REQUIRED -> {
-                "Security Update Required"
-            }
-            BiometricManager.BIOMETRIC_ERROR_UNSUPPORTED -> {
-                "Un Supported"
-            }
-            BiometricManager.BIOMETRIC_STATUS_UNKNOWN -> {
-                "Unknown"
-            }
-            BiometricManager.BIOMETRIC_SUCCESS -> {
-                "Available"
-            }
+            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE -> "Hardware Unavailable"
+            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> "Biometric None Enrolled"
+            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE -> "No Biometric Hardware"
+            BiometricManager.BIOMETRIC_ERROR_SECURITY_UPDATE_REQUIRED -> "Security Update Required"
+            BiometricManager.BIOMETRIC_ERROR_UNSUPPORTED -> "Un Supported"
+            BiometricManager.BIOMETRIC_SUCCESS -> "Available"
+            BiometricManager.BIOMETRIC_STATUS_UNKNOWN -> "Unknown"
             else -> "Unknown"
         }
     }
@@ -142,8 +110,7 @@ class MainActivity : AppCompatActivity() {
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             Intent(Settings.ACTION_BIOMETRIC_ENROLL).apply {
                 putExtra(
-                    Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED,
-                    authenticators
+                    Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED, authenticators
                 )
             }
         } else {
